c36d287c8ba3d5a685b12357b0a824f500cd8276
Bipul Kumar Singh <bipulsinghkashyap@gmail.com>
2026-02-13 21:43:03 +0530

    correct token

--- src/middleware.py
+++ src/middleware.py
@@ -2,8 +2,13 @@
 
 import jwt
 from typing import Annotated
-from fastapi import Request, Header, HTTPException #, Depends
+from fastapi import Request, Header, HTTPException, status #, Depends
 # from fastapi.security import OAuth2PasswordBearer
+from jwt import (
+    ExpiredSignatureError,
+    InvalidTokenError,
+    DecodeError
+)
 from src.logger import logger
 
 
@@ -21,18 +26,41 @@ def token_decoder(token):
 
 
 # def authentication(token: Annotated[str, Depends(oauth2_scheme)], request: Request):
-def authentication(token: Annotated[str, Header()], request: Request):
+def authentication(token: Annotated[str, Header(alias="Authorization")], request: Request):
     """ Authentication Middleware """
     try:
         logger.info(f"Token received: {token}")
-        # token = token.split()
         request.state.context = token_decoder(token)
         logger.info(f"Token decoded! {request.state.context}")
+    except ExpiredSignatureError:
+        logger.warning("JWT expired")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "code": "TOKEN_EXPIRED",
+                "message": "Access token expired"
+            }
+        )
+
+    except (InvalidTokenError, DecodeError):
+        logger.warning("Invalid JWT")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "code": "INVALID_TOKEN",
+                "message": "Invalid access token"
+            }
+        )
+
     except Exception as ex:
         logger.exception(ex)
-        raise HTTPException(status_code=400, detail={
-            "data": "Please provide valid token!",
-            "error": True
-        })
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "code": "AUTH_ERROR",
+                "message": "Authentication failed"
+            }
+        )
+
     
 # response.headers["X-API-Version"] = "0.0.1"
